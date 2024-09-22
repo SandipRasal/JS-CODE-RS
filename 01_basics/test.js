b4cbe19c7ae4c3a38931b74fbdc7e95300650e39
SandipRasal <144212314+SandipRasal@users.noreply.github.com>
2024-09-22 18:44:37 +0000

    done

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,26 @@
 console.log("hello world");
+const accountId = 1234   //do not change
+let accountEmail = "sandip@gml.com"
+var accountPasssword ="123459876"  //not used
+accountCity = "Mumbai  "
+
+console.log(accountId);
+
+/* NOTE:
+prefer not to used var
+becouse of issue in block scope and functional scope
+*/
+
+
+console.table([accountPasssword ,accountId,accountEmail,accountCity])
+
+
+//02_Basic
+
+"use strict"  // treat all JS code as newer version
+
+// alert(3 + 3)  //we are using nodejs ,not browser
+//// documentation => MDN,tc.39 sites
+
+
+ 
